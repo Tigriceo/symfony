79f3a714c69cdd1169bfcc79cb3b56d2429cd748
Tigriceo <koval.ss92@gmail.com>
2019-07-03 20:06:29 +0300

    init3

---
+++ src/Admin/ProductAdmin.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skillup_student
+ * Date: 03.07.19
+ * Time: 19:39
+ */
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+
+class ProductAdmin extends AbstractAdmin
+{
+
+    protected function configureListFields(ListMapper $list)
+    {
+       $list
+           ->addIdentifier('id')
+           ->addIdentifier('name')
+       ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+            ->add('id')
+            ->add('name')
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $form)
+    {
+       $form
+           ->add('id')
+           ->add('name')
+       ;
+    }
+
+
+}
